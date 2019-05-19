0db9e54b9f2bfedf32f8ec352b7821fe93322353
Helmy <chanhelmy@hotmail.com>
2019-05-19 06:08:32 +0000

    Add search function for products and start a new app checkout and make some changes to css

--- products/views.py
+++ products/views.py
@@ -4,6 +4,14 @@ from cart.forms import AddToCartForm
 # Create your views here.
 def all_products(request):
     add_to_cart_form = AddToCartForm
-    all_products = Product.objects.all()
-    return render(request, "products.html", {"all_products": all_products,'form':add_to_cart_form})
+    if 'search_terms' in request.GET:
+         search_terms = request.GET.get('search_terms')
+         all_products=Product.objects.filter(name__contains=search_terms)
+    else:
+        all_products = Product.objects.all()
+    return render(request, "products.html", {
+                "all_products": all_products,
+                'form':add_to_cart_form
+        
+})
 
