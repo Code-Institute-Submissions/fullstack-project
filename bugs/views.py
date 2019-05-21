a3b493ffdc4ec4e1adbd1139cd345c46b34263fc
Helmy <chanhelmy@hotmail.com>
2019-05-21 06:57:46 +0000

    Added bugs html, urls py for bugs app, register bugs model into bugs admin and added views.py for get all bugs feature

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect, reverse,HttpResponse
+from .models import Bug, BugComment
+from django.contrib.auth.decorators import login_required
+# from .forms import BugForm, BugCommentForm
 # Create your views here.
+@login_required()
+def all_bugs(request):
+    all_bugs = Bug.objects.all()
+    return render(request, "bugs.html", {'all_bugs':all_bugs} )
