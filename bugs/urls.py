a96b93ad058fbecd99e092e199f6ed08692ac795
Helmy <chanhelmy@hotmail.com>
2019-05-21 07:37:49 +0000

    Add upvote feature in bugs app

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_bugs
+from .views import all_bugs, upvote_bug
 
 urlpatterns = [
-    url(r'^$', all_bugs, name='bugs')
+    url(r'^$', all_bugs, name='bugs'),
+    url(r'^upvote/(?P<id>\d+)$', upvote_bug, name='upvote_bug' )
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -7,3 +7,13 @@ from django.contrib.auth.decorators import login_required
 def all_bugs(request):
     all_bugs = Bug.objects.all()
     return render(request, "bugs.html", {'all_bugs':all_bugs} )
+
+@login_required()
+def upvote_bug(request, id):
+    """
+    A view that upvotes the selected bug
+    """
+    bug = Bug.objects.get(pk=id)
+    bug.upvotes += 1
+    bug.save()
+    return redirect(all_bugs)
