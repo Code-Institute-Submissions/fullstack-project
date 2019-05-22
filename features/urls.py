8b235b4be65da867ed5cca7cbcf60a55eea737d7
Helmy <chanhelmy@hotmail.com>
2019-05-22 04:29:20 +0000

    Add upvote function in features app

--- features/urls.py
+++ features/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_features
+from .views import all_features, upvote_feature
 
 urlpatterns = [
-     url(r'^$', all_features, name='features')
+     url(r'^$', all_features, name='features'),
+     url(r'^upvote/(?P<id>\d+)$', upvote_feature, name='upvote_feature' )
 ]
--- features/views.py
+++ features/views.py
@@ -8,3 +8,13 @@ from django.contrib.auth.decorators import login_required
 def all_features(request):
     all_features = Feature.objects.all()
     return render(request, "features.html", {'all_features':all_features} )
+
+@login_required()
+def upvote_feature(request, id):
+    """
+    A view that upvotes the selected bug
+    """
+    feature = Feature.objects.get(pk=id)
+    feature.upvotes += 1
+    feature.save()
+    return redirect(all_features)
